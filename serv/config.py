0c3a5003919d41396f9607ae5a4b0f03410413d2
Maier Christian (GS/EIC3) <Christian.Maier3@de.bosch.com>
2025-05-04 22:20:41 +0200

    Minor changes (print output etc.) config.py names changed to demo data base

--- serv/config.py
+++ serv/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-trans_dict = {'Controller': 'Flugleiter', 'Pilot': 'Schlepppilot', 'Teacher': 'Fluglehrer', 'Winch': 'Windenfahrer'}
+trans_dict = {'Controller': 'Betriebsleiter', 'Pilot': 'Schlepppilot', 'Teacher': 'Fluglehrer', 'Winch': 'Windenfahrer'}
 vf_csv_header = ['Dienstbezeichnung', 'Dienstbeginn', 'Dienstende', 'Max. Personen', 'Person', 'Person2'] # import header definition from www.vereinsflieger.de
 
 
@@ -11,15 +11,15 @@ def set_config(df_user):
     # - Individual time periods per user has to be configured as holidays
 
     # Controller
-    df_user.loc['Controller'].at['Flamm, Sigrud', 'max'] = 4.0
-    df_user.loc['Controller'].at['Rand, Peter', 'pref_day'] = 'Sun'
+    df_user.loc['Controller'].at['Blonde, The', 'max'] = 4.0
+    df_user.loc['Controller'].at['Grand, Peter', 'pref_day'] = 'Sun'
 
     # Pilot
     #df_user.loc['Pilot', 'max'] = 6.0 # defines end of season
-    df_user.loc['Pilot'].at['Duffy, Rob', 'max'] = 7.0
+    df_user.loc['Pilot'].at['Turkish, Jason', 'max'] = 5.0
     
     # Winch
-    df_user.loc['Winch'].at['Dan, Man', 'max'] = 6.0
-    df_user.loc['Winch'].at['Dan, Man', 'b-e'] = 0.45
+    df_user.loc['Winch'].at['Simpson, Homer', 'max'] = 6.0
+    df_user.loc['Winch'].at['Simpson, Homer', 'b-e'] = 0.45
 
     return df_user
